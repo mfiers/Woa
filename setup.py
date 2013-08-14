a7f7e48bb4826d6add44ebba4f65a2c39684c774
Mark Fiers <mark.fiers.42@gmail.com>
2013-08-14 14:38:31 +0200

    setup script

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""
+Woa setup script
+"""
+
+from setuptools import setup
+
+requires = [
+    'flask',
+    'argparse'
+]
+
+with open('VERSION') as F:
+    version = F.read().strip()
+
+packages = [
+    'woa',
+]
+
+setup(name='woa',
+      version=version,
+      description='Woa - a web interface for Moa',
+      author='Mark Fiers',
+      author_email='mark.fiers.42@gmail.com',
+      url='http://mfiers.github.com/Woa/',
+      # entry_points=entry_points,
+      packages=packages,
+      include_package_data=True,
+      # data_files=data_files,
+      zip_safe=False,
+      install_requires=requires,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Unix Shell',
+          'Topic :: Scientific/Engineering :: Bio-Informatics'
+      ])
+
+
